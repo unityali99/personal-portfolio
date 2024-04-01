aa6bda542679c1556f361b3a8bd11bb25dce5586
Ali Ahmadi <mrkiller1383@gmail.com>
2024-04-02 03:19:47 +0330

    Home page picture: Added the homepage picture and styled it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,34 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text, VStack } from "@chakra-ui/react";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div className="text-6xl">{"I'm Ali Ahmadi"}</div>
-      <Box
-        as="button"
-        color={"white"}
-        bg={"blueviolet"}
-        py={3}
-        px={7}
-        className="rounded-md"
-      >
-        Download CV
-      </Box>
+    <main className="flex flex-row justify-around">
+      <VStack align={"start"} className="space-y-5">
+        <Box className="space-y-3">
+          <Text className="text-6xl">{"I'm Ali Ahmadi"}</Text>
+          <Text className="text-xl font-light">
+            {"Node.js Web Developer - React.js & React Native"}
+          </Text>
+        </Box>
+        <Box
+          as="button"
+          color={"white"}
+          bg={"blueviolet"}
+          py={3}
+          px={7}
+          className="rounded-md mx-auto"
+        >
+          Download CV
+        </Box>
+      </VStack>
+      <Image
+        alt="me"
+        src={"/216259.jpg"}
+        width={400}
+        height={100}
+        className="-translate-y-28 -z-10"
+      />
     </main>
   );
 }
--- src/components/NavbarMenu.tsx
+++ src/components/NavbarMenu.tsx
@@ -1,4 +1,4 @@
-import { Text, VStack } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
 
